cedfdec6259d4de26f21b9797c0ab72958fc6f28
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-08-12 08:05:26 +0700

    Initialize socket.io server, Send message API

--- server/routes/messageRouter.js
+++ server/routes/messageRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.js";
-import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/messageController.js";
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js";
 
 const messageRouter = express.Router();
 
 messageRouter.post("/users", protectRoute, getUsersForSidebar);
 messageRouter.post("/:id", protectRoute), getMessages;
 messageRouter.post("/mark/:id", protectRoute, markMessageAsSeen);
+messageRouter.post("/send/:id", protectRoute, sendMessage);
 
 export default messageRouter;
--- server/server.js
+++ server/server.js
@@ -11,10 +11,37 @@ import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRouter.js";
+import { Server } from "socket.io";
+import { Socket } from "dgram";
 
 // Create Express app and HTTP server
 const app = express();
 const server = http.createServer(app)
+
+// Initialize socket.io server
+export const io = new Server({
+    cors: {origin: "*"}
+})
+
+// Store online users
+export const userSocketMap = {}; // {userId: socketId}
+
+// Socket.io connection handler
+io.on("connection", (socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    //Emit online user to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
 
 // Middleware setup
 app.use(express.json({limit: "4mb"}));
