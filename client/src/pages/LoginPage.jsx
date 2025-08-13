301c38208d729dffb197da9dd1e088e6e98c6bfb
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-08-13 10:01:33 +0700

    Fix css

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -23,8 +23,8 @@ const LoginPage = () => {
   }
   
   return (
-    <div className='min-h-screen bg-cover bg-center flex items-center justify-cent
-    gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2x1'>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center
+    gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
 
       {/* -------- left side -------- */}
 
@@ -33,7 +33,7 @@ const LoginPage = () => {
       {/* -------- right side -------- */}
 
       <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
-        <h2 className='font-medium text-2x1 flex justify-between items-center'>
+        <h2 className='font-medium text-2xl flex justify-between items-center'>
           {currState}
           {isDataSubmitted && <img onClick={()=> setIsDataSubmitted(false)}
           src={assets.arrow_icon} alt='' className='w-5 cursor-pointer' />}
